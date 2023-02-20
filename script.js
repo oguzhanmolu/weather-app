4aecd0d20ea597900e948a02d778fb3a0f127aaf
oguzhanmolu <oguzhanmolu@gmail.com>
2023-02-20 14:28:00 +0300

    added process and display data functions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-'strict mode';
+let arr = [];
+('strict mode');
 const inputSearch = document.getElementById('input-search');
 const buttonSearch = document.querySelector('.icon-search');
 const weatherCondition = document.getElementById('weather-condition');
@@ -6,22 +7,30 @@ const locationInfo = document.getElementById('location-info');
 const temperatureInfo = document.getElementById('temperature-info');
 const iconWeatherCondition = document.getElementById('icon-weather-condition');
 
-// Get weather data from API
+// Get weather data from API,
+// then process the data with "processWeatherData" function
 async function getWeatherData(location) {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=f4507264ae2d4096a6d124819231902 &q=${location}`
+      `http://api.weatherapi.com/v1/forecast.json?key=f4507264ae2d4096a6d124819231902 &q=${location}`,
+      { mode: 'cors' }
     );
     const weatherData = await response.json();
-    console.log(weatherData);
+    processWeatherData(weatherData);
   } catch {
     alert('City not found');
   }
 }
 
-getWeatherData('Auckland');
+// Process the data
+function processWeatherData(data) {
+  console.log(data);
+}
 
-// Event listeners for search bar
+// Display the data
+function displayWeatherData() {}
+
+// Search bar events
 buttonSearch.addEventListener('click', () => {
-  inputSearch.value = '';
+  getWeatherData(inputSearch.value);
 });
