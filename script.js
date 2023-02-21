bb6e66793c319e59740c0fb1ee90889658441160
oguzhanmolu <oguzhanmolu@gmail.com>
2023-02-21 16:07:18 +0300

    added feelslike humidity wind speed and date infos

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let arr = [];
-('strict mode');
+'strict mode';
+// Variables
 const inputSearch = document.getElementById('input-search');
 const buttonSearch = document.querySelector('.icon-search');
 const weatherCondition = document.getElementById('weather-condition');
@@ -8,44 +8,60 @@ const temperatureInfo = document.getElementById('temperature-info');
 const groupWeatherCondition = document.getElementById(
   'group-weather-condition'
 );
+const dateInfo = document.getElementById('date-info');
 const iconWeatherCondition = document.getElementById('icon-weather-condition');
-
+const feelsLikeInfo = document.getElementById('feels-like-info');
+const humidityInfo = document.getElementById('humidity-info');
+const windInfo = document.getElementById('wind-info');
 // Get weather data from API,
 // then process the data with "processWeatherData" function
 async function getWeatherData(location) {
-  try {
-    const response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=f4507264ae2d4096a6d124819231902 &q=${location}`,
-      { mode: 'cors' }
-    );
-    const weatherData = await response.json();
-    displayWeatherData(processWeatherData(weatherData));
-  } catch {
-    alert('City not found');
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=f4507264ae2d4096a6d124819231902 &q=${location}`,
+    { mode: 'cors' }
+  );
+  if (response.status == 400 || !location) {
+    inputSearch.value = '';
+    return alert('Location not found');
   }
+
+  const weatherData = await response.json();
+  console.log(weatherData);
+  displayWeatherData(processWeatherData(weatherData));
 }
 
-// Process the data, create a new object with the input data(city name)
+// Create a new object with the input location
 function processWeatherData(object) {
   const weatherObject = {
     condition: object.current.condition.text,
-    conditionSrc: object.current.condition.icon,
+    iconSrc: object.current.condition.icon,
     cityName: object.location.name,
     countryName: object.location.country,
+    lastUpdated: object.current.last_updated,
     temperature: object.current.temp_c,
+    feelsLike: object.current.feelslike_c,
+    humidity: object.current.humidity,
+    windSpeed: object.current.wind_kph,
   };
   return weatherObject;
 }
 
-// Display the data in text content
+// Get curren date as hour:minute
+const getDate = () => `${new Date().getHours()}:${new Date().getMinutes()}`;
+
+// Display the processed data as text
 function displayWeatherData(object) {
-  iconWeatherCondition.src = object.conditionSrc;
+  iconWeatherCondition.src = object.iconSrc;
   weatherCondition.textContent = object.condition;
   locationInfo.textContent = `${object.cityName}, ${object.countryName}`;
+  dateInfo.textContent = getDate();
   temperatureInfo.textContent = object.temperature;
   groupWeatherCondition.style.display = 'flex';
   groupWeatherCondition.style.alignItems = 'center';
-  inputSearch.textContent = '';
+  inputSearch.value = '';
+  feelsLikeInfo.textContent = `Feels Like: ${object.feelsLike}°C`;
+  humidityInfo.textContent = `Humidity: ${object.humidity}%`;
+  windInfo.textContent = `Wind Speed: ${object.windSpeed} km/h`;
 }
 
 // Searchbar event listener
@@ -57,5 +73,5 @@ document.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') getWeatherData(inputSearch.value);
 });
 
-// Placeholder display for start
+// Placeholder display
 getWeatherData('Istanbul');
